cbf17cfa65086c323ee83829971d1fe199ac6aea
Boris Tereshchenko <betereshchenko@avito.ru>
2018-03-23 00:41:44 +0300

    Fixed property name in StringReverserTest

--- tests/Day1/ReverseTest.php
+++ tests/Day1/ReverseTest.php
@@ -13,18 +13,18 @@ use PHPUnit\Framework\TestCase;
 
 class StringReverserTest extends TestCase
 {
-    private $reverter;
+    private $reverse;
     public function setUp(){
-        $this->reverter = new StringReverser();
+        $this->reverse = new StringReverser();
     }
     public function test_reverse_qwerty(){
-        $this->assertEquals('ytrewq', $this->reverter->solution('qwerty'));
+        $this->assertEquals('ytrewq', $this->reverse->solution('qwerty'));
     }
     public function test_reverse_qwerty_cyryllic(){
-        $this->assertEquals('некуцй', $this->reverter->solution('йцукен'));
+        $this->assertEquals('некуцй', $this->reverse->solution('йцукен'));
     }
     public function test_reverse_int_to_string(){
-        $this->assertEquals('12345', $this->reverter->solution(54321));
+        $this->assertEquals('12345', $this->reverse->solution(54321));
     }
 
 }
